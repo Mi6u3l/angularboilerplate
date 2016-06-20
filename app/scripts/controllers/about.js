dcb0aab111d9de878b3c2009b82d284de47f7d80
Mi6u3l <miguel.bgomes@gmail.com>
2016-06-20 08:34:26 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularboilerplateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularboilerplateApp
+ */
+angular.module('angularboilerplateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularboilerplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularboilerplateApp
+ */
+angular.module('angularboilerplateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
